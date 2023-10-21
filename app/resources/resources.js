ba38375b4e810ea06137bfa49efc9063c945c07c
Sai krishnan P <sai.krishnan663@gmail.com>
2023-10-21 22:17:58 +0530

    add: resource.js
    Signed-off-by: Sai krishnan P <sai.krishnan663@gmail.com>

---
+++ app/resources/resources.js
@@ -0,0 +1,38 @@
+export const songsDirectory = [
+    {
+      "id": 1,
+      "name": "Remember Me",
+      "artist": "Miguel",
+      "src": "",
+      "img": ""
+    },
+    {
+      "id": 2,
+      "name": "Wavin flag",
+      "artist": "K'naan",
+      "src": "",
+      "img": ""
+    },
+    {
+      "id": 3,
+      "name": "People",
+      "artist": "Libianca",
+      "src": "",
+      "img": ""
+    },
+    {
+      "id": 4,
+      "name": "Lucid dreams",
+      "artist": "Juice WRLD",
+      "src": "",
+      "img": ""
+    },
+    {
+      "id": 5,
+      "name": "Blinding lights",
+      "artist": "Weekend",
+      "src": "",
+      "img": ""
+    }
+  ]
+  
