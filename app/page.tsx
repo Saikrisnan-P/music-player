d09dffb7a87fd5af89678a3d83eb86988147fdab
Sai krishnan P <sai.krishnan663@gmail.com>
2023-10-27 22:18:39 +0530

    add header
    Signed-off-by: Sai krishnan P <sai.krishnan663@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
+'use client';
 import './styles/styles.css'
 
+import type { RootState } from '@/redux/store';
+import { songsDirectory } from '@/app/resources/resources'
+import { useSelector, useDispatch } from 'react-redux';
+
+import './styles/styles.css'
+import React, { useEffect, useState } from 'react';
+import { Music, setDb } from '@/redux/features/musicDB-slice';
+
 const HomePage = () => {
+
+  const musicDB = useSelector((state: RootState) => state.musicDB.value);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(setDb(songsDirectory as Music[]));
+    console.log('eff called')
+  }, []);
+
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+      setLoaded(true)
+  }, []);
+
   return (
-    <div className="homeContainer">HomePage</div>
+    <div className='home-container'>
+      <header>
+            <h1 className='animate-charcter'>Music Player</h1>
+        </header>
+    </div>
   )
 }
 
